73ba5fc288633c77e96c02275decdc221ae3abef
mikeheul <mickael.murmann@gmail.com>
2025-03-29 17:05:12 +0100

    Add form types for Course and Training entities

---
+++ src/Form/TrainingType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Training;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrainingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('startDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('endDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('price')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Training::class,
+        ]);
+    }
+}
