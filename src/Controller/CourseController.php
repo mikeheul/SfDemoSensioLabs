bdbacb379be3b64b1cbdf5ad540d5553f3a7420b
mikeheul <mickael.murmann@gmail.com>
2025-03-29 18:44:13 +0100

    Add new course creation functionality with form handling and template integration

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Course;
+use App\Form\CourseType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CourseController extends AbstractController
 {
@@ -13,6 +17,25 @@ final class CourseController extends AbstractController
     {
         return $this->render('course/index.html.twig', [
             'controller_name' => 'CourseController',
+        ]);
+    }
+    
+    #[Route('/course/new', name: 'new_course')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CourseType::class, new Course());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $course = $form->getData();
+
+            $em->persist($course);
+            $em->flush();
+            
+            return $this->redirectToRoute('app_course');
+        }
+        return $this->render('course/new.html.twig', [
+            'formAddCourse' => $form,
         ]);
     }
 }
