364837f351e3451f3c988e203603f593f4fe4555
mikeheul <mickael.murmann@gmail.com>
2025-03-29 23:54:54 +0100

    Add Users management to Admin Dashboard and update UserCrudController with role selection

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Entity\Course;
 use App\Entity\Training;
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\Admin\TrainingCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+#[IsGranted('ROLE_ADMIN')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
@@ -37,6 +40,7 @@ class DashboardController extends AbstractDashboardController
             MenuItem::section('Tranings / Courses'),
             MenuItem::linkToCrud('Trainings', 'fa fa-tags', Training::class),
             MenuItem::linkToCrud('Courses', 'fa fa-file-text', Course::class),
+            MenuItem::linkToCrud('Users', 'fa fa-users', User::class),
         ];
     }
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,10 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +16,18 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email'),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'Admin' => 'ROLE_ADMIN',
+                    'Teacher' => 'ROLE_TEACHER',
+                    'Student' => 'ROLE_STUDENT',
+                ])
+                ->allowMultipleChoices() // Permet de sélectionner plusieurs rôles
+                ->setHelp('Laissez vide si vous ne voulez pas attribuer de rôles spécifiques. ROLE_USER est toujours attribué par défaut.')
         ];
     }
-    */
 }
