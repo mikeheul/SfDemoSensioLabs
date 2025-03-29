35b03be81014d1dda2eced15c0cb3aaa5036455f
mikeheul <mickael.murmann@gmail.com>
2025-03-29 18:19:47 +0100

    Enhance CourseType and TrainingType forms with specific field types for better validation and user experience

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -6,14 +6,16 @@ use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [])
+            ->add('description', TextareaType::class, [])
         ;
     }
 
--- src/Form/TrainingType.php
+++ src/Form/TrainingType.php
@@ -3,24 +3,28 @@
 namespace App\Form;
 
 use App\Entity\Training;
+use Doctrine\DBAL\Types\FloatType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TrainingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('startDate', null, [
+            ->add('title', TextType::class, [])
+            ->add('description', TextareaType::class, [])
+            ->add('startDate', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('endDate', null, [
+            ->add('endDate', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('price')
+            ->add('price', FloatType::class, [])
         ;
     }
 
