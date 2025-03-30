04e62e4af770188e96d4c5a4ed8d0ccf8cb422e7
mikeheul <mickael.murmann@gmail.com>
2025-03-30 18:02:34 +0200

    Add training voter for subscription and student viewing permissions; update template checks accordingly

--- src/Security/Voter/TrainingVoter.php
+++ src/Security/Voter/TrainingVoter.php
@@ -11,16 +11,30 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 final class TrainingVoter extends Voter
 {
     public const CREATE = 'TRAINING_CREATE';
+    public const SUBSCRIBE = 'TRAINING_SUBSCRIBE';
+    public const VIEW_STUDENTS = 'TRAINING_VIEW_STUDENTS';
 
     public function __construct(private Security $security) {}
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return in_array($attribute, [self::CREATE]) && $subject === null;
+        // return in_array($attribute, [self::CREATE]) && $subject === null;
+        return in_array($attribute, [self::SUBSCRIBE, self::VIEW_STUDENTS]) && $subject instanceof Training;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        return $this->security->isGranted('ROLE_ADMIN');
+        // return $this->security->isGranted('ROLE_ADMIN');
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        return match ($attribute) {
+            self::SUBSCRIBE => in_array('ROLE_STUDENT', $user->getRoles()),
+            self::VIEW_STUDENTS => $this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_TEACHER'),
+            default => false
+        };
     }
 }
