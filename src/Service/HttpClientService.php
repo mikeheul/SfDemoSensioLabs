e67ce2b009ff3b8c1976f7aec81d8a3ec3bff8a1
mikeheul <mickael.murmann@gmail.com>
2025-04-01 09:04:18 +0200

    Add API controller and service with initial template for API response

---
+++ src/Service/HttpClientService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Service;
+
+class HttpClientService
+{
+    public function __construct()
+    {
+        // TODO: Inject dependencies here if needed
+    }
+
+    public function doSomething(): void
+    {
+        // TODO: Implement service logic
+    }
+}
