2656e2d19a14c69417f1fb05758ed6affa25169f
mikeheul <mickael.murmann@gmail.com>
2025-04-22 16:34:19 +0200

    Refactor HomeController routes for locale handling and add translation keys for welcome messages in English, Spanish, and French

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'app_home_redirect')]
+    public function redirectToHome(): Response
+    {
+        return $this->redirectToRoute('app_home', [
+            '_locale' => 'en',
+        ]);
+    }
+
+    #[Route('/{_locale}/home', name: 'app_home', requirements: ['_locale' => 'en|fr|es'], defaults: ['_locale' => 'en'])]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', []);
