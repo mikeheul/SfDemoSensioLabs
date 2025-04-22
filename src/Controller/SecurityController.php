8bec95de61c2318cf33ffdb7a5659973ce9a5599
mikeheul <mickael.murmann@gmail.com>
2025-04-22 16:48:18 +0200

    Add multilingual support for registration and login forms with locale routing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route('/{_locale}', requirements: ['_locale' => 'en|fr|es'], defaults: ['_locale' => 'en'])]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
